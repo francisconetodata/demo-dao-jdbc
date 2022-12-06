f9d31adb74010ecd5b5fdf5096a1855dcbc1d13c
Francisco Neto <francisconetodata@gmail.com>
2022-12-06 20:47:19 -0300

    final program test

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,6 +1,8 @@
 package model.dao;
 
 import java.util.List;
+
+import model.entities.Department;
 import model.entities.Seller;
 
 
@@ -8,8 +10,9 @@ import model.entities.Seller;
 public interface SellerDao {
 	void insert(Seller obj);
 	void update(Seller obj);
-	void deleteById(Seller id);
-	Seller findById(Seller id);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
+	List<Seller> findByDepartment(Department department);
 	
 }

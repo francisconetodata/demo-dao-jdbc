7d34705562b7e1921c9be158d5dcd22f278afda5
Francisco Neto <francisconetodata@gmail.com>
2022-12-06 20:15:36 -0300

    seller ent class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1,"Books");
 		System.out.println(obj);
+		Seller seller = new Seller(11,"Books","Teste@gmail.com",
+				new Date(),3650.5,obj);
+		System.out.println(seller);
 	}
 
 }
